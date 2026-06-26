27a790a4924fd72b5a80c21936637f2192ef649c
Miquel Ferrer Llompart <miquel@jericommerce.com>
2026-06-26 15:38:30 +0200

    Use only enabled Trello Power-Up capabilities (#22)
    * Revert "Handle enabled Trello admin capabilities (#21)"
    
    This reverts commit 3614d50743973147dcaf2b75bd6f4f5153535641.
    
    * Revert "Handle enabled Trello board buttons (#20)"
    
    This reverts commit bdce88bbcb25f9da69162e082cfe1c7b380b4ede.
    
    * Revert "Handle Trello card detail badges (#19)"
    
    This reverts commit a9df410f58a38d6c850cb2b9f2906ae883d84e45.
    
    * Guard Trello capability registration

--- src/connector.ts
+++ src/connector.ts
@@ -1,4 +1,4 @@
-import { getCardBadges, getCardDetailBadges } from './capabilities/card-badges';
+import { getCardBadges } from './capabilities/card-badges';
 import { getCardButtons } from './capabilities/card-buttons';
 import { getCardBackSection } from './capabilities/card-back-section';
 import { HOLDED_ICON_URL } from './icons';
@@ -6,15 +6,9 @@ import { TRELLO_APP_KEY } from './config';
 
 window.TrelloPowerUp.initialize(
   {
-    'board-buttons': () => [],
     'card-buttons': (t: unknown) => getCardButtons(t),
     'card-badges': (t: unknown) => getCardBadges(t),
-    'card-detail-badges': (t: unknown) => getCardDetailBadges(t),
     'card-back-section': (t: unknown) => getCardBackSection(t, HOLDED_ICON_URL),
-    'list-sorters': () => [],
-    'save-attachment': () => ({
-      callback: () => undefined,
-    }),
   },
   {
     appKey: TRELLO_APP_KEY,
